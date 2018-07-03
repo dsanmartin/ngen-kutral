0e7325d3f4c1dc428d63f5a5c29832e67c4a6a64
Daniel San Martin <dsanmartinreyes@gmail.com>
2018-07-03 00:02:06 -0400

    Some experiments

--- test.py
+++ test.py
@@ -2,9 +2,6 @@ import wildfire
 import numpy as np
 import matplotlib.pyplot as plt
 import plots as p
-#from scipy import interpolate
-
-  
 #%%
 # Domain: [-1, 1]^2 x [0, T*dt]
 #M, N = 16, 16 # Resolution
@@ -16,19 +13,9 @@ ya, yb = -100, 100 # y domain limit
 x = np.linspace(xa, xb, N) # x domain
 y = np.linspace(ya, yb, M) # y domain
 t = np.linspace(0, dt*L, L) # t domain
-
-dx = x[1]-x[0]
     
 # Meshes for initial condition plots
 X, Y = np.meshgrid(x, y)
-
-# TODO: define parameters in function of real conditions
-#T_env = 300
-#Ea = 83.68
-#A = 1e9
-#rho = 1e2
-#C = 1
-#k = 1
 
 # Topography test
 xi = 0#1e-1
@@ -79,23 +66,101 @@ kappa = 1e-1#5e-3 # diffusion coefficient
 epsilon = 3e-1#3e-1 # inverse of activation energy
 upc = 3e1 # u phase change
 q = 1e-0 # reaction heat
-alpha = 1e-1#1e-3#1e1#1e-1 # natural convection
+alpha = 1e-1 # natural convection
+#%% Asensio 2002 experiment
+#M, N = 128, 128
+M, N = 256, 256
+L = 2000 # Timesteps
+dt = 1e-3 # dt
+xa, xb = -100, 100 # x domain limit
+ya, yb = -100, 100 # y domain limit
+x = np.linspace(xa, xb, N) # x domain
+y = np.linspace(ya, yb, M) # y domain
+t = np.linspace(0, dt*L, L) # t domain
+
+# Temperature initial condition
+u0 = lambda x, y: 6e1*np.exp(-2e-2*((x+60)**2 + (y+60)**2)) 
+
+# Fuel initial condition
+b0 = lambda x, y: np.round(np.random.uniform(size=(x.shape)), decimals=2)#x*0 + 1 #S(x+.25, y+.25) #x*0 + 1
+
+# Wind effect
+gamma = 1
+w1 = lambda x, y, t: gamma * np.cos(np.pi/4 + x*0)
+w2 = lambda x, y, t: gamma * np.sin(np.pi/4 + x*0)
+W = (w1, w2)
+
+# Topography test
+xi = 0#1e-1
+top = lambda x, y: 1e0*(np.exp(-2e-3*((x+30)**2 + (y)**2)) + np.exp(-2e-3*((x-30)**2 + y**2)))
+t1 = lambda x, y: xi*-4e-1**((x+30) * np.exp(-2e-3*((x+30)**2 + y**2)) + (x-30) * np.exp(-2e-3*((x-30)**2 + y**2)))
+t2 = lambda x, y: xi*-4e-1*(y * np.exp(-2e-3*((x+30)**2 + y**2)) + y * np.exp(-2e-3*((x-30)**2 + y**2)))
+T = (t1, t2)
+
+# Vector
+v1 = lambda x, y, t: w1(x, y, t) + t1(x, y)
+v2 = lambda x, y, t: w2(x, y, t) + t2(x, y)
+V = (v1, v2)
+
+# Parameters
+kappa = 1e-1 # diffusion coefficient
+epsilon = 3e-1#3e-2 # inverse of activation energy
+upc = 3e0 # u phase change
+q = 1 # reaction heat
+alpha = 1e-3 # natural convection
+
+#%% Mell 2006 experiment
+M, N = 64, 64
+L = 1000 # Timesteps
+dt = 1e-2 # dt
+xa, xb = -100, 100 # x domain limit
+ya, yb = -100, 100 # y domain limit
+x = np.linspace(xa, xb, N) # x domain
+y = np.linspace(ya, yb, M) # y domain
+t = np.linspace(0, dt*L, L) # t domain
+
+# Temperature initial condition
+u0 = lambda x, y: 1e2*np.exp(-2e-2*((x+80)**2 + (y)**2)) 
+
+# Fuel initial condition
+b0 = lambda x, y: x*0 + 1
+
+# Wind effect
+gamma = 1
+w1 = lambda x, y, t: gamma * np.cos(0 + x*0)
+w2 = lambda x, y, t: gamma * np.sin(0 + x*0)
+W = (w1, w2)
+
+# Topography test
+xi = 0#1e-1
+top = lambda x, y: 1e0*(np.exp(-2e-3*((x+30)**2 + (y)**2)) + np.exp(-2e-3*((x-30)**2 + y**2)))
+t1 = lambda x, y: xi*-4e-1**((x+30) * np.exp(-2e-3*((x+30)**2 + y**2)) + (x-30) * np.exp(-2e-3*((x-30)**2 + y**2)))
+t2 = lambda x, y: xi*-4e-1*(y * np.exp(-2e-3*((x+30)**2 + y**2)) + y * np.exp(-2e-3*((x-30)**2 + y**2)))
+T = (t1, t2)
+
+# Vector
+v1 = lambda x, y, t: w1(x, y, t) + t1(x, y)
+v2 = lambda x, y, t: w2(x, y, t) + t2(x, y)
+V = (v1, v2)
+
+# Parameters
+kappa = 1e1 # diffusion coefficient
+epsilon = 3e-1#3e-2 # inverse of activation energy
+upc = 1e0 # u phase change
+q = 1e1 # reaction heat
+alpha = 1e-2 # natural convection
+#%%
+# Meshes for initial condition plots
+X, Y = np.meshgrid(x, y)
 
 # Plot initial conditions
-s = 8
-#plotScalar(X, Y, u0, "Initial condition", plt.cm.jet)
-#plotScalar(X, Y, b0, "Fuel", plt.cm.Oranges)
-#plotScalar(X, Y, top, "Topography", plt.cm.Oranges)
-#plotField(X[::s,::s], Y[::s,::s], T, "Topography Gradient")
-#p.plotField(X[::s,::s], Y[::s,::s], W, "Wind", 0)
-#plotField(X[::s,::s], Y[::s,::s], V, "Topography + Wind", 0)
 p.plotIC(X, Y, u0, b0, V, W, T, top)
 
 # Parameters for the model
 parameters = {
     'u0': u0, 
     'beta0': b0,
-    'v': V,
+    'v': W,
     'kappa': kappa, 
     'epsilon': epsilon, 
     'upc': upc, 
@@ -107,18 +172,52 @@ parameters = {
     'sparse': True
 }
 
-#%%
 ct = wildfire.fire(parameters)
 #%%
 # Finite difference in space
 U, B = ct.solvePDE('fd', 'rk4')
-#%%
 #ct.plots(U, B)
 
 #%% PLOT JCC
-p.plotJCC(t, X, Y, U, B, W)
+p.plotJCC(t, X, Y, U, B, W, save=False)
 
-  #%% Times experiment
+#%%
+plt.figure(figsize=(10, 10))
+#plt.contourf(X, Y, U[0])
+#plt.contourf(X, Y, U[250])
+#plt.contourf(X, Y, U[500])
+#plt.contourf(X, Y, U[750])
+#plt.contourf(X, Y, U[-1])
+#levels = np.arange(3, 5, 0.1)
+#plt.contour(X, Y, B[0])
+#plt.contour(X, Y, B[250], levels=levels)
+#plt.contour(X, Y, B[500], levels=levels)
+#plt.contour(X, Y, B[750], levels=levels)
+#plt.contour(X, Y, B[-1], levels=levels)
+#Bs = (1-B[0]) + (1-B[250]) + (1-B[500]) + (1-B[750]) + (1-B[-1])
+#plt.contourf(X, Y, Bs)#, levels=levels)
+
+dif_b = (B[1:] - B[:-1]) / dt
+levels = np.arange(-0.07, -0.04, 0.01)
+print(levels)
+#plt.contourf(dif_b[500])
+plt.contourf(X, Y, dif_b[0], levels=levels)
+plt.contourf(X, Y, dif_b[250], levels=levels)
+plt.contourf(X, Y, dif_b[500], levels=levels)
+plt.contourf(X, Y, dif_b[750], levels=levels)
+plt.contourf(X, Y, dif_b[-1], levels=levels)
+
+
+#levels = np.arange(0.5, 1.0, 0.1)
+#plt.imshow(B[250], origin="lower", extent=[-100, 100, -100, 100])
+#plt.imshow(B[500])
+#plt.imshow(B[750])
+#plt.imshow(B[-1])
+#plt.contour(B[250], levels, origin="lower", extent=[-100, 100, -100, 100])
+plt.colorbar()
+plt.show()
+
+#%% Times experiment
 x_t = np.array([0.8, 0.4, 0.0, -0.4, -0.8])
 y_t = np.array([-0.8, -0.4, 0.0, 0.4, 0.8])
 
@@ -156,87 +255,6 @@ for i in range(5):
 #plt.imshow(B[-1])
 plt.imshow(times_burnt, extent=[-1, 1, -1, 1])
 plt.colorbar()
-
-
-#%%
-s = 10
-f, axarr = plt.subplots(4, 2, sharex='col', sharey='row', figsize=(24, 16))
-#f.tight_layout()
-f.subplots_adjust(left=0.3, right=0.6)
-
-p00 = axarr[0, 0].imshow(U[0], origin='lower', cmap=plt.cm.jet, alpha=0.9, 
-     vmin=np.min(U), vmax=np.max(U), extent=[-1, 1, -1, 1])
-axarr[0, 0].quiver(X[::s,::s], Y[::s,::s], w1(X[::s,::s], Y[::s,::s], t[0]), w2(X[::s,::s], Y[::s,::s], t[0])) 
-
-p01 = axarr[0, 1].imshow(B[0], origin='lower', cmap=plt.cm.Oranges, alpha=0.9, 
-     vmin=np.min(B), vmax=np.max(B), extent=[x[0], x[-1], y[0], y[-1]])
-
-p10 = axarr[1, 0].imshow(U[333], origin='lower', cmap=plt.cm.jet, alpha=0.9, 
-     vmin=np.min(U), vmax=np.max(U), extent=[x[0], x[-1], y[0], y[-1]])
-axarr[1, 0].quiver(X[::s,::s], Y[::s,::s], w1(X[::s,::s], Y[::s,::s], t[333]), w2(X[::s,::s], Y[::s,::s], t[333])) 
-
-
-p11 = axarr[1, 1].imshow(B[333], origin='lower', cmap=plt.cm.Oranges, alpha=0.9, 
-     vmin=np.min(B), vmax=np.max(B), extent=[x[0], x[-1], y[0], y[-1]])
-
-p20 = axarr[2, 0].imshow(U[666], origin='lower', cmap=plt.cm.jet, alpha=0.9, 
-     vmin=np.min(U), vmax=np.max(U), extent=[x[0], x[-1], y[0], y[-1]])
-axarr[2, 0].quiver(X[::s,::s], Y[::s,::s], w1(X[::s,::s], Y[::s,::s], t[666]), w2(X[::s,::s], Y[::s,::s], t[666])) 
-
-
-p21 = axarr[2, 1].imshow(B[666], origin='lower', cmap=plt.cm.Oranges, alpha=0.9, 
-     vmin=np.min(B), vmax=np.max(B), extent=[x[0], x[-1], y[0], y[-1]])
-
-p30 = axarr[3, 0].imshow(U[-1], origin='lower', cmap=plt.cm.jet, alpha=0.9, 
-     vmin=np.min(U), vmax=np.max(U), extent=[x[0], x[-1], y[0], y[-1]])
-axarr[3, 0].quiver(X[::s,::s], Y[::s,::s], w1(X[::s,::s], Y[::s,::s], t[-1]), w2(X[::s,::s], Y[::s,::s], t[-1])) 
-
-
-p31 = axarr[3, 1].imshow(B[-1], origin='lower', cmap=plt.cm.Oranges, alpha=0.9, 
-     vmin=np.min(B), vmax=np.max(B), extent=[x[0], x[-1], y[0], y[-1]])
-
-
-for ax in axarr.flat:
-    #ax.set(xlabel='x', ylabel='y')
-    ax.set_xlabel(r"$x$", fontsize=14)
-    ax.set_ylabel(r"$y$", fontsize=14)
-# Hide x labels and tick labels for top plots and y ticks for right plots.
-for ax in axarr.flat:
-    ax.label_outer()
-    
-cb1 = plt.colorbar(p00, ax=axarr[0,0], fraction=0.046, pad=0.04)
-cb2 = plt.colorbar(p01, ax=axarr[0,1], fraction=0.046, pad=0.04)
-cb3 = plt.colorbar(p10, ax=axarr[1,0], fraction=0.046, pad=0.04)
-cb4 = plt.colorbar(p11, ax=axarr[1,1], fraction=0.046, pad=0.04)
-cb5 = plt.colorbar(p20, ax=axarr[2,0], fraction=0.046, pad=0.04)
-cb6 = plt.colorbar(p21, ax=axarr[2,1], fraction=0.046, pad=0.04)
-cb7 = plt.colorbar(p30, ax=axarr[3,0], fraction=0.046, pad=0.04)
-cb8 = plt.colorbar(p31, ax=axarr[3,1], fraction=0.046, pad=0.04)
-
-#axarr[3, 1].xticks(fontsize=14)
-axarr[0,0].tick_params(axis='both', which='major', labelsize=12)
-axarr[1,0].tick_params(axis='both', which='major', labelsize=12)
-axarr[2,0].tick_params(axis='both', which='major', labelsize=12)
-axarr[3,0].tick_params(axis='both', which='major', labelsize=12)
-axarr[3,1].tick_params(axis='both', which='major', labelsize=12)
-#plt.tick_params(axis='both', which='minor', labelsize=20)
-
-cb1.ax.tick_params(labelsize=12)
-cb2.ax.tick_params(labelsize=12)
-cb3.ax.tick_params(labelsize=12)
-cb4.ax.tick_params(labelsize=12)
-cb5.ax.tick_params(labelsize=12)
-cb6.ax.tick_params(labelsize=12)
-cb7.ax.tick_params(labelsize=12)
-cb8.ax.tick_params(labelsize=12)
-
-plt.rc('axes', labelsize=12)
-#plt.savefig('simulation.eps', format='eps', dpi=200, transparent=True, bbox_inches='tight', pad_inches=0)
-plt.show()
-
-
-
-
 #%%
 # Chebyshev in space
 Wc, Bc = ct.solvePDE('cheb', 'rk4')
@@ -244,26 +262,40 @@ Wc, Bc = ct.solvePDE('cheb', 'rk4')
 ct.plots(Wc, Bc, True)
 
 #%%
-W_1024 = np.load('data/last_W_1024.npy')
-W_512 = np.load('data/last_W_512.npy')
-W_256 = np.load('data/last_W_256.npy')
-W_128 = np.load('data/last_W_128.npy')
+U_1024 = np.load('experiments/convergence/last_u_1024.npy')
+U_512 = np.load('experiments/convergence/last_u_512.npy')
+U_256 = np.load('experiments/convergence/last_u_256.npy')
+U_128 = np.load('experiments/convergence/last_u_128.npy')
+U_64 = np.load('experiments/convergence/last_u_64.npy')
+
+B_1024 = np.load('experiments/convergence/last_b_1024.npy')
+B_512 = np.load('experiments/convergence/last_b_512.npy')
+B_256 = np.load('experiments/convergence/last_b_256.npy')
+B_128 = np.load('experiments/convergence/last_b_128.npy')
+B_64 = np.load('experiments/convergence/last_b_64.npy')
 #%%
-#errors = np.array([
-#    np.max(np.abs(W_128 - W_1024[::8, ::8])),
-#    np.max(np.abs(W_256 - W_1024[::4, ::4])),
-#    np.max(np.abs(W_512 - W_1024[::2, ::2]))
-#    ])
-errors = np.array([
-    np.linalg.norm((W_128 - W_1024[::8, ::8]).flatten(), np.inf),
-    np.linalg.norm((W_256 - W_1024[::4, ::4]).flatten(), np.inf),
-    np.linalg.norm((W_512 - W_1024[::2, ::2]).flatten(), np.inf),
+errors_u = np.array([
+    np.linalg.norm((U_64 - U_1024[::16, ::16]).flatten(), np.inf),
+    np.linalg.norm((U_128 - U_1024[::8, ::8]).flatten(), np.inf),
+    np.linalg.norm((U_256 - U_1024[::4, ::4]).flatten(), np.inf),
+    np.linalg.norm((U_512 - U_1024[::2, ::2]).flatten(), np.inf),
     ])
-h = np.array([2/(2**i) for i in range(7, 10)])
+
+errors_b = np.array([
+    np.linalg.norm((B_64 - B_1024[::16, ::16]).flatten(), np.inf),
+    np.linalg.norm((B_128 - B_1024[::8, ::8]).flatten(), np.inf),
+    np.linalg.norm((B_256 - B_1024[::4, ::4]).flatten(), np.inf),
+    np.linalg.norm((B_512 - B_1024[::2, ::2]).flatten(), np.inf),
+    ])
+  
+h = np.array([200/(2**i) for i in range(6, 10)])
 #%%
-plt.plot(h, errors, '-x')
+#plt.plot(h, errors_u, 'b-x')
+plt.plot(h, errors_b, 'r-o')
 plt.xlabel("h")
 plt.grid(True)
+plt.xscale('log')
+plt.yscale('log')
 plt.show()
 
 #%%
@@ -277,8 +309,6 @@ plt.grid(True)
 plt.xscale('log')
 plt.yscale('log')
 plt.show()
-
-
 #%%
 plt.contour(X,Y,U[0])
 plt.contour(X,Y,U[3000])
