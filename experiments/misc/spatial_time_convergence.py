0ea030933c00df8f02d2862bff909287e7ff6373
Daniel <dsanmartinreyes@gmail.com>
2018-11-27 11:41:11 -0300

    Script for cluster

--- experiments/misc/spatial_time_convergence.py
+++ experiments/misc/spatial_time_convergence.py
@@ -1,18 +1,25 @@
 import numpy as np
 import pathlib
+import sys
 from wildfire.fire import Fire
 from wildfire import plots as p
 import matplotlib.pyplot as plt
 from wildfire.diffmat import chebyshevMatrix
-#%%
-BASE_DIR = "experiments/misc/spatial_time_convergence/"
+
+if len(sys.argv) < 4:
+  print("Arguments error")
+  sys.exit()
+
+BASE_DIR = sys.argv[1]#"/user/d/dsanmart/wildfires/ngen-kutral/experiments/misc/spatial_time_convergence/"
+N = int(sys.argv[2])
+dt = float(sys.argv[3])
 
 # Parameters for the model
 parameters = {
     'u0': lambda x, y: 6 * np.exp(-2e2*((x+.65)**2 + (y+.65)**2)), 
     'beta0': lambda x, y: x*0 + 1,
-    'v': (lambda x, y, t: 2 * np.cos(np.pi/4 + x*0 + 1e-2*t), 
-          lambda x, y, t: 2 * np.sin(np.pi/4 + x*0 + 1e-2*t)),
+    'v': (lambda x, y, t: 2 * np.cos(np.pi/4 + x*0), 
+          lambda x, y, t: 2 * np.sin(np.pi/4 + x*0)),
     'kappa': 1e-2, 
     'epsilon': 1e-1, 
     'upc': 3, 
@@ -22,56 +29,21 @@ parameters = {
     'show': False,
     'complete': False
 }
-#%%
-Ns = np.array([2 ** j for j in range(4, 11)])
-Ts = np.array([i for i in range(4, 12, 2)])
-TsN = np.array([10**i+1 for i in range(4, 12, 2)])
-ii = 0
-for ts in Ts:
-  for N in Ns:
-    
-    dt_str = '1e-' + str(ts)
-    dt = float(dt_str)
-    print("Simulation using: dt = {0}, N = {1}".format(dt, N))
-    print(TsN[ii])
-    
-    dir_name = BASE_DIR + dt_str + "/fd/"
 
-    pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True) 
-    
-    parameters['x'] = np.linspace(-1, 1, N)
-    parameters['y'] = np.linspace(-1, 1, N)
-    parameters['t'] = np.linspace(0, 1, TsN[ii])    
-    
-    ct = Fire(parameters)
-    # Solve
-    # Uc, Bc = ct.solvePDE('cheb', 'last')
-    Ufd, Bfd = ct.solvePDE('fd', 'last')
-    np.save(dir_name + "U" + str(N), Ufd)
-  ii += 1
-#%%
+#dt_str = '1e-' + str(ts)
+#dt = float(dt_str)
+#print("Simulation using: dt = {0}, N = {1}".format(dt, N))
 
-Ns = np.array([2 ** j for j in range(4, 11)])
-Ts = np.array([i for i in range(4, 12, 2)])
+dir_name = BASE_DIR + "/" + sys.argv[3] + "/fd/"
 
-errors_ = np.zeros((len(Ts), len(Ns) - 1))
+pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True) 
 
-for i in range(len(Ts)):
-  U1024 = np.load('experiments/misc/spatial_time_convergence/1e-' + str(Ts[i]) + '/fd/U1024.npy')
-  for j in range(len(Ns)-1):
-    Utmp = np.load('experiments/misc/spatial_time_convergence/1e-' + str(Ts[i]) + '/fd/U' + str(Ns[j]) + '.npy')
-    errors_[i, j] = np.linalg.norm((U1024[::1024 // Ns[j],:: 1024 // Ns[j]] - Utmp).flatten(), np.inf)
-#%%   
-plt.figure(figsize=(8, 6))
-plot = plt.imshow(errors_[::,::-1], cmap=plt.cm.coolwarm)
-plt.xticks(np.arange(len(Ns)-1), map(lambda N: '2/' + str(N) , Ns[-2::-1]))
-plt.yticks(np.arange(len(Ts)), map(lambda T: '1e-' + str(T), Ts))
-cbar = plt.colorbar(plot, fraction=0.046, pad=0.04)
-cbar.set_label(r"$||U_{ref} - U_{N}||_{\infty}$", fontsize=14)
-plt.xlabel(r"$h$", fontsize=14)
-plt.ylabel(r"$\Delta t$", fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=12)
-plt.savefig('st_conv.pdf', format='pdf')
-plt.show()
-#%%
-plt.plot(Ns[:-1], errors_[0])
+parameters['x'] = np.linspace(-1, 1, N)
+parameters['y'] = np.linspace(-1, 1, N)
+parameters['t'] = np.linspace(0, 1, int(1/dt))    
+
+ct = Fire(parameters)
+# Solve
+# Uc, Bc = ct.solvePDE('cheb', 'last')
+Ufd, Bfd = ct.solvePDE('fd', 'last')
+np.save(dir_name + "U" + str(N), Ufd)
